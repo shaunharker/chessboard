121cf4da4541e78c2874367a70972c5faa2b0fc5
Shaun Harker <sharker81@gmail.com>
2022-05-15 18:52:43 -0400

    output moves database

--- src/moves.cpp
+++ src/moves.cpp
@@ -1,3 +1,7 @@
+// moves.cpp
+// Shaun Harker
+// BSD ZERO CLAUSE LICENSE
+
 #include <cstdint>
 #include <array>
 #include <iostream>
@@ -91,7 +95,7 @@ struct ThreeByteMove {
       (trk() == '1'))) return false;
 
     // to change castling rights there are nine cases:
-    //  1. king move
+    //  1. king move from its home square
     //  2. a1 rook captured by black
     //  3. h1 rook captured by black
     //  4. a8 rook captured by white
@@ -102,17 +106,40 @@ struct ThreeByteMove {
     //  9. h8 rook moved black
     // White could capture an unmoved a8 rook with its unmoved a1 rook,
     // and similar scenarios, so the cases aren't mutually exclusive.
-    bool kingmove = (sp() == 'K');
+
+    // it isn't possible to remove castling rights via a Rf8 x Rh8 because the enemy king would be in check. Similarly for other exceptions
+
+    bool kingmove = (sp() == 'K') && (sr() == (white ? 7 : 0)) && (sc()==4);
     bool a1rookcapture = (cp() == 'R') && (ti() == 56) && !white;
     bool a8rookcapture = (cp() == 'R') && (ti() == 0) && white;
     bool h1rookcapture = (cp() == 'R') && (ti() == 63) && !white;
     bool h8rookcapture = (cp() == 'R') && (ti() == 7) && white;
-    bool a1rookmove = (sp() == 'R') && (si() == 56) && white;
-    bool a8rookmove = (sp() == 'R') && (si() == 0) && !white;
-    bool h1rookmove = (sp() == 'R') && (si() == 63) && white;
-    bool h8rookmove = (sp() == 'R') && (si() == 7) && !white;
-    if (kcr() && !(kingmove || h1rookcapture || h1rookmove || h8rookcapture || h8rookmove)) return false;
-    if (qcr() && !(kingmove || a1rookcapture || a1rookmove || a8rookcapture || a8rookmove)) return false;
+
+    bool a1rookmove = (sp() == 'R') && (si() == 56) && white && ((sc() == 0) || (sc() == 1) || (sc() == 2) || (sc() == 3));
+    bool a8rookmove = (sp() == 'R') && (si() == 0) && !white && ((sc() == 0) || (sc() == 1) || (sc() == 2) || (sc() == 3));
+    bool h1rookmove = (sp() == 'R') && (si() == 63) && white && ((sc() == 5) || (sc() == 6) || (sc() == 7));
+    bool h8rookmove = (sp() == 'R') && (si() == 7) && !white && ((sc() == 5) || (sc() == 6) || (sc() == 7));
+    if (kcr() && !(kingmove || h1rookmove || h8rookmove)) {
+      if (h1rookcapture || h8rookcapture) {
+        // exclude moves implying king is en prise
+        if ((sp()=='R') && (sc()<6)) return false;
+        if ((sp()=='Q') && (sr() == tr()) && (sc()<6)) return false;
+        if ((sp()=='K') && ((sr() == tr()) || (sc() == tc()))) return false;
+      } else {
+        return false;
+      }
+    }
+    if (qcr() && !(kingmove || a1rookmove || a8rookmove)) {
+      if (a1rookcapture || a8rookcapture) {
+        // exclude moves implying king is en prise
+        if ((sp()=='R') && (sc() > 2)) return false;
+        if ((sp()=='Q') && (sr() == tr()) && (sc() > 2)) return false;
+        if ((sp()=='N') && (srk() == (white ? '7' : '2')) && (sc() == 2)) return false;
+        if ((sp()=='K') && ((sr() == tr()) || (sc() == tc()))) return false;
+      } else {
+          return false;
+      }
+    }
 
     if (sp() == 'P') {
       // pawns can only move forward one rank at a time,
@@ -200,13 +227,14 @@ struct ThreeByteMove {
     }
 
    if (sp() == 'K') {
-     // king move cannot lose castling rights unless starting on home square
-     if ((sf() != 'e') && (kcr() || qcr())) return false;
-     if ((srk() != (white ? '1' : '8')) && (kcr() || qcr())) return false;
      // if kingside castle, must be losing kingside rights
      if ((sf() == 'e') && (srk() == (white ? '1' : '8')) && (tf() == 'g') && (trk() == (white ? '1' : '8')) && !kcr()) return false;
      // if queenside castle, must be losing queenside rights
-     if ((sf() == 'e') && (srk() == (white ? '1' : '8')) && (tf() == 'g') && (trk() == (white ? '1' : '8')) && !qcr()) return false;
+     if ((sf() == 'e') && (srk() == (white ? '1' : '8')) && (tf() == 'c') && (trk() == (white ? '1' : '8')) && !qcr()) return false;
+     // the weirdest moves: (objectively, too. the last to have to debug and add code to handle) king takes rook losing castling rights.
+     // Only diagonal/antidiagonal captures could possibly occur during play.
+     if ((cp() == 'R') && kcr() && (trk() == (white ? '8' : '1')) && (tf() == 'h') && !((srk() == (white ? '7' : '2')) && (sf() == 'g'))) return false;
+     if ((cp() == 'R') && qcr() && (trk() == (white ? '8' : '1')) && (tf() == 'a') && !((srk() == (white ? '7' : '2')) && (sf() == 'b'))) return false;
      // castling cannot capture, must be properly positioned
      if ((sqr(sc()) + sqr(tc()) > 1 + 2*sc()*tc())) {
        if (!((tf() == 'g') && kcr()) && !((tf() == 'c') && qcr())) return false;
@@ -216,12 +244,15 @@ struct ThreeByteMove {
        if (trk() != (white ? '1' : '8')) return false;
      }
      // kings move to neighboring squares
-     if ((sqr(sc()) + sqr(tc()) + sqr(sr()) + sqr(tr()) > 2*(1 + sc()*tc() + sr()*tr())) && !(kcr() || qcr())) return false;
+     if ((sqr(sc()) + sqr(tc()) + sqr(sr()) + sqr(tr()) >
+         2*(1 + sc()*tc() + sr()*tr())) &&
+         !((sc() == 4) && (sr() == (white ? 7 : 0)) && (tr()==sr()) &&
+           (((tc()==2) && qcr()) || ((tc()==6) && kcr())))) return false;
    }
-   if (sp() == 'Q') {
-     std::cout << sp() << sf() << srk() << ((cp() != '.') ? "x" : "") << tp() << tf() << trk() << " captured: " << cp() << " X = " << X << " " <<
-     (c() ? "b" : "w") << " spi: " << int(spi()) << " tpi: " << int(tpi()) << " cpi: " << int(cpi()) << " si: " << int(si()) << " ti: " << int(ti()) << " cr: " << (kcr() ? "K" : "") << (qcr() ? "Q" : "") << "\n";
-   }
+   // if (sp() == 'K') {
+   //   std::cout << sp() << sf() << srk() << ((cp() != '.') ? "x" : "") << tp() << tf() << trk() << " captured: " << cp() << " X = " << X << " " <<
+   //   (c() ? "b" : "w") << " spi: " << int(spi()) << " tpi: " << int(tpi()) << " cpi: " << int(cpi()) << " si: " << int(si()) << " ti: " << int(ti()) << " cr: " << (kcr() ? "K" : "") << (qcr() ? "Q" : "") << "\n";
+   // }
    return true;
   }
   // advance to next legitimate move
@@ -236,9 +267,15 @@ int main(int arc, char * argv []) {
   uint32_t bcnt = 0;
   uint32_t qcnt = 0;
   uint32_t kcnt = 0;
+  std::cout << "turn, sp, sf, srk, tp, tf, trk, cp, kcr, qcr\n";
   for ( uint32_t i = 0; i < 256*256*256; ++i) {
     ThreeByteMove tbm(i);
     if (tbm.feasible()) {
+      std::cout << (tbm.c() ? '1' : '0') << ", " << tbm.sp() << ", " <<
+        tbm.sf() << ", " << tbm.srk() << ", " << tbm.tp() << ", " <<
+        tbm.tf() << ", " << tbm.trk() << ", " <<
+        tbm.cp() << ", " << (tbm.kcr() ? '1' : '0') << ", " <<
+        (tbm.qcr() ? '1' : '0') << "\n";
       ++ cnt;
       switch(tbm.sp()){
         case 'P': ++pcnt; break;
@@ -250,12 +287,20 @@ int main(int arc, char * argv []) {
       }
     }
   }
-  std::cout << cnt << "\n";
-  std::cout << "P " << pcnt << "\n";
-  std::cout << "N " << ncnt << "\n";
-  std::cout << "R " << rcnt << "\n";
-  std::cout << "B " << bcnt << "\n";
-  std::cout << "Q " << qcnt << "\n";
-  std::cout << "K " << kcnt << "\n";
+  std::cerr << cnt << "\n";
+  std::cerr << "P " << pcnt << "\n";
+  std::cerr << "N " << ncnt << "\n";
+  std::cerr << "R " << rcnt << "\n";
+  std::cerr << "B " << bcnt << "\n";
+  std::cerr << "Q " << qcnt << "\n";
+  std::cerr << "K " << kcnt << "\n";
   return 0;
 }
+
+// here's the breakdown:
+// P: (8+(8+14*5)*5+(8*4+14*4*4))*2 + 28 + 16 == 1352
+// N: ((2*4+3*8+4*16)+(4*4 + 6*16)+8*16)*2*6 - 2*4*2 - 3*4*2 - 4*8*2 + 8 - 2 == 3934
+// R: (8*5+6*6)*32 + 48*(2*5+12*6)*2 + (3*5+6*6)*2 + (4*5+6*6)*2 + (7+8)*2 == 6524
+// B: (7*28+9*20+11*12+13*4)*2*6-7*32+28 = 3934
+// Q: (21*28+23*20+25*12+27*4)*6*2 - 21*16*2 + 21*4 - 11*2 = 16862
+// K: (3*4+5*24+8*36)*6*2-(3*4+5*12)*2 + 8 + 4 + (3*6+2*5)*2*3 = 5076
